0334013208e7c3a700086583832d1b97239059f4
Lukas <lhange42@charlottesvilleschools.org>
2022-02-15 19:43:05 +0000

    testing code

--- Code/Python/PiCode/measure.py
+++ Code/Python/PiCode/measure.py
@@ -1,3 +1,12 @@
-import Adafruit_INA260 from Libraries
 import time
+import board
+import adafruit_ina260
+
+i2c = board.I2C()
+ina260 = adafruit_ina260.INA260(i2c)
+while True:
+    print("Current: %.2f Voltage: %.2f Power:%.2f"
+        %(ina260.current, ina260.voltage, ina260.power))
+    time.sleep(1)
+
 
