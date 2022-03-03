60bf580a97a1adde92d579f9663efc464f70a31b
Lukas <lhange42@charlottesvilleschools.org>
2022-03-03 20:06:46 +0000

    Finished Code

--- Code/Python/PiCode/LCDDisplay.py
+++ Code/Python/PiCode/LCDDisplay.py
@@ -7,12 +7,11 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 i2c = board.I2C()
-RST = 24
+RST = board.24
 ina260 = adafruit_ina260.INA260(i2c) # accelerometer setup
-disp = adafruit_ssd1306.SSD1306_I2C(128,64,i2c, addr=0x3d) # dislay setup
-disp.begin()
-disp.clear()
-disp.display()
+disp = adafruit_ssd1306.SSD1306_I2C(128,64,i2c, addr=0x3d,reset = RST) # dislay setup
+disp.fill(0)
+disp.show()
 
 width = disp.width
 height = disp.height 
---
+++ Code/Python/PiCode/testCode.py
@@ -0,0 +1,45 @@
+import adafruit_ssd1306
+import digitalio
+import board
+import busio
+import time
+import adafruit_ina260
+
+i2c = board.I2C()
+ina260 = adafruit_ina260.INA260(i2c)
+
+i2c = busio.I2C(board.SCL, board.SDA)
+from PIL import Image, ImageDraw, ImageFont
+reset_pin = digitalio.DigitalInOut(board.D24) # any pin!
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr = 0x3d, reset=reset_pin)
+
+
+oled.fill(0)
+oled.show()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new("1", (oled.width, oled.height))
+
+draw = ImageDraw.Draw(image)
+font = ImageFont.load_default()
+while True:
+
+    print("Current: %.2f Voltage: %.2f Power:%.2f"
+        %(ina260.current, ina260.voltage, ina260.power))
+    text = "Voltage:" + str(ina260.current / 4) 
+    (font_width, font_height) = font.getsize(text)
+    draw.text(
+    (0 + 20 , 0 + 20),
+    text,
+    font=font,
+    fill=255,
+)
+    print(ina260.current)
+# Display image
+    oled.image(image)
+    oled.show()
+    time.sleep(1)
+    draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
+    oled.image(image)
+    oled.show
